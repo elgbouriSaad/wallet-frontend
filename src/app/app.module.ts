710dc77fb26f77047e01d0027b14b36935835541
Soufiane Oukattou <117850230+SoufianeOukattou@users.noreply.github.com>
2023-12-10 19:11:46 +0100

    Implement login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginPageComponent,
+ 
+
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ButtonModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
